491bc5efddb08d5b10e1102a1e37d1b3668f6e6d
Tom-Sloan <tsloan81@gmail.com>
2021-04-05 14:42:20 -0400

    small changes

--- bot.py
+++ bot.py
@@ -12,7 +12,6 @@ access_token = environ['ACCESS_TOKEN']
 access_token_secret = environ['ACCESS_TOKEN_SECRET']
 
 quotes_user_id = environ['QUOTES_USER_ID']
-
 quotes_token = environ['QUOTES_TOKEN']
 
 
@@ -72,7 +71,7 @@ def create_tweet():
     return tweet
 
 def tweet_quote():
-    interval = 60*24*24
+    interval = 10
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
@@ -82,7 +81,9 @@ def tweet_quote():
     # api.update_status(tweet)
 
     while True:
+        print('Getting Tweet')
         tweet = create_tweet()
+        print(tweet)
         api.update_status(tweet)
         time.sleep(interval) 
         
